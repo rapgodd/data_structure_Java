22c27bbdf5f3f52789ef672e97b684891f7457a5
GiyeonChoi <bdcgy22@daum.net>
2024-09-28 21:10:16 +0900

    스택 자료구조 코딩테스트 문제

--- src/main/java/com/giyeon/data_structure/backjoon/Stack.java
+++ src/main/java/com/giyeon/data_structure/backjoon/Stack.java
@@ -1,14 +1,14 @@
 package com.giyeon.data_structure.backjoon;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class Stack {
 
     public static void main(String[] args) {
 
         Deque<Integer> stack = new ArrayDeque<>();
+        Integer beforeNum = 0;
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -19,14 +19,35 @@ public class Stack {
             arr[i] = requiredNum;
         }
 
+        //리스트에 +-저장
+        List<String> answerList = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             Integer integer = arr[i];
-            for (int j = 1; j <= integer; j++) {
-                stack.push(j);
-                System.out.println("+");
+            if(i==0){
+                for (int j = 1; j <= integer; j++) {
+                    stack.push(j);
+                    answerList.add("+");
+                }
+                beforeNum = integer;
+            }else if(arr[i]>beforeNum){
+                for (int j = beforeNum + 1; j <= arr[i]; j++) {
+                    stack.push(j);
+                    answerList.add("+");
+                }
+                beforeNum = integer;
+            }else{
+                if(stack.peek().equals(arr[i])){
+                }else {
+                    System.out.println("NO");
+                    return;
+                }
             }
             stack.pop();
-            System.out.println("-");
+            answerList.add("-");
+        }
+        for (String answer : answerList) {
+            System.out.println(answer);
         }
 
     }
