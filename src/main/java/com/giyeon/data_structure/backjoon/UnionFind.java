9949b82f105aa5f5180b47c47670ee7cf8a07f8e
GiyeonChoi <bdcgy22@daum.net>
2024-10-08 22:31:28 +0900

    부분집합 문제 수정

--- src/main/java/com/giyeon/data_structure/backjoon/UnionFind.java
+++ src/main/java/com/giyeon/data_structure/backjoon/UnionFind.java
@@ -21,6 +21,9 @@ public class UnionFind {
         }
 
         //주어진 명령과 값 두개를 보면서 경우에 따라 연산을 수행한다.
+        //인덱스 = 값
+        //값 = 부모
+        //a , b는 값이닌까 인덱스이다
         for (int i = 0; i < m; i++) {
             String calculation = br.readLine();
             String[] commandAndNum = calculation.split(" ");
@@ -30,33 +33,36 @@ public class UnionFind {
 
             if (command == 0) {
                 if(a<b){
-                    for (int j = 0; j < arr.length; j++) {
-                        if(arr[j]==b){
-                            arr[j] = a;
-                        }
-                    }
+                  //b인덱스의 최상위 부모를 a 부모의 아래에 둔다
+                    int parent = getParent(b, arr);
+                    arr[parent] = getParent(a, arr);
                 }else if(a>b){
-                    for (int j = 0; j < arr.length; j++) {
-                        if(arr[j]==a){
-                            arr[j] = b;
-                        }
-                    }
-                }else{
-
+                    //a인덱스의 최상위 부모를 b부모의 아래로 둔다.
+                    int parent = getParent(a, arr);
+                    arr[parent] = getParent(b, arr);
                 }
-
             }else {//1인경우
-                if (arr[a] == arr[b]) {
+                //조건 : a의 최상위 부모와 b의 최상위 부모가 같으면
+                //결과 : Yes출력
+                //조건 : a의 최상위 부모와 b의 최상위 부모가 다르면
+                //결과 : No 출력
+                if(getParent(a,arr)==getParent(b,arr)){
                     bw.write("YES\n");
-                }else {
+                }else{
                     bw.write("NO\n");
                 }
             }
-
         }
         bw.flush();
 
         br.close();
         bw.close();
+    }
+    public static int getParent(int b, int[] arr) {
+        if(b==arr[b]){
+            return b;
+        }else{
+            return arr[b] = getParent(arr[b], arr);
+        }
     }
 }
