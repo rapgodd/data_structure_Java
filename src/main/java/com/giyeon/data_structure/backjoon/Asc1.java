6fdf68af5695e01a8156b76ebb3e0790620e4f86
GiyeonChoi <bdcgy22@daum.net>
2024-10-22 23:55:52 +0900

    정렬

---
+++ src/main/java/com/giyeon/data_structure/backjoon/Asc1.java
@@ -0,0 +1,29 @@
+package com.giyeon.data_structure.backjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Asc1 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int initialNum = Integer.parseInt(br.readLine());
+        Integer[] numArr = new Integer[initialNum];
+
+        for (int i = 0; i < initialNum; i++) {
+            int givenNum = Integer.parseInt(br.readLine());
+            numArr[i] = givenNum;
+        }
+
+        Arrays.sort(numArr);
+        for (Integer integer : numArr) {
+            bw.write(integer.toString() + "\n");
+        }
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
