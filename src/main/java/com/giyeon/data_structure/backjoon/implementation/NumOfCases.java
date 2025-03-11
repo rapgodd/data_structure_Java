54abc61d9a2fa329e34f1f46901ad5ea9d6ae74f
GiyeonChoi <bdcgy22@daum.net>
2025-03-11 23:36:49 +0900

    이중 반복문 O(n^2)을 조건을 추가하여 단축
    이중 반복문은 O(n^2)의 시간복잡도를 가진다.
    이 시간 복잡도를 줄이기 위해
    조건을 반복문에 추가해 주었고 성공적으로 실행시간을 단축시켰다.

---
+++ src/main/java/com/giyeon/data_structure/backjoon/implementation/NumOfCases.java
@@ -0,0 +1,43 @@
+package com.giyeon.data_structure.backjoon.implementation;
+
+public class NumOfCases {
+
+    public int v1(int n){
+        int count = 1;
+
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+
+            for(int j=i; j<=n; j++){
+                sum += j;
+                if(sum>n){break;}
+                if(sum==n){count+=1; break;}
+            }
+        }
+
+        return count;
+
+    }
+    
+
+    public int v2(int n){
+        int count = 1;
+
+        for(int i=1; i<=n; i++){
+            int sum = 0;
+
+            if(n/2 < i){
+                break;
+            }
+
+            for(int j=i; j<=n; j++){
+                sum += j;
+                if(sum>n){break;}
+                if(sum==n){count+=1; break;}
+            }
+        }
+
+        return count;
+
+    }
+}
